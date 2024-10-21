e5256d43e1d21e0714c8f81bc798820df45607db
roman395 <goruskinroma@gmail.com>
2024-10-21 17:01:48 +0300

    first commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/AutonomeRed1.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode.drive;
+
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+
+@Autonomous
+@Config
+public class AutonomeRed1 extends LinearOpMode {
+    SampleMecanumDrive mec;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        mec=new SampleMecanumDrive(hardwareMap);
+
+
+        TrajectorySequence e = mec.trajectorySequenceBuilder(new Pose2d(-12, -58, Math.toRadians(0)))
+
+                .lineToLinearHeading(new Pose2d(60,-40,Math.toRadians(-70)))
+                .waitSeconds(1)
+                .turn(Math.toRadians(160))
+
+                .waitSeconds(0.5)
+                .turn(Math.toRadians(-160))
+
+                .waitSeconds(1)
+                .splineToLinearHeading(new Pose2d(-24,-12,Math.toRadians(-180)),Math.toRadians(0))
+
+
+                .build();
+
+
+        waitForStart();
+        mec.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        mec.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        mec.setPoseEstimate(new Pose2d(-12,-58));
+        mec.followTrajectorySequence(e);
+
+
+
+
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/AutonomeRed2.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode.drive;
+
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+
+@Autonomous
+@Config
+public class AutonomeRed2 extends LinearOpMode {
+    SampleMecanumDrive mec;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        mec=new SampleMecanumDrive(hardwareMap);
+
+
+        TrajectorySequence e = mec.trajectorySequenceBuilder(new Pose2d(12, -58, Math.toRadians(0)))
+                .lineToLinearHeading(new Pose2d(30,-60,Math.toRadians(-45)))
+
+                .waitSeconds(1)
+
+                .splineToLinearHeading(new Pose2d(36,-24,Math.toRadians(0)),Math.toRadians(45))
+
+                .waitSeconds(0.5)
+                .splineTo(new Vector2d(30,-60),Math.toRadians(-45))
+
+                .waitSeconds(1)
+                .lineToLinearHeading(new Pose2d(24,-12,Math.toRadians(0)))
+
+                .build();
+
+
+        waitForStart();
+        mec.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        mec.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        mec.setPoseEstimate(new Pose2d(12,-58));
+        mec.followTrajectorySequence(e);
+
+
+
+
+
+    }
+}
